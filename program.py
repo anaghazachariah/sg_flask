7d22e0db11d06fe6481c90fcbfe8d63ad98538db
anagha zachariah c <anaghazachariah@gmail.com>
2021-04-19 10:19:00 +0530

    update

--- program.py
+++ program.py
@@ -4,6 +4,6 @@ app=Flask(__name__)#creating your Flask application instance with the name app
 
 @app.route("/")#pass '/' to signify that this function respond to main URL
 def home():
-    return render_template("index.html")#connects with html page
+    return render_template("index.html",process_list="RCG",table_list="RCG,MI,E2K,PRC",antecedent="NOT_PEC_AND_QTZ_SOURCE_APPN_IDS",consequent="SECONDBIT=_APP_ORGIN_RCG")#connects with html page
 if __name__=="__main__":
    app.run()#run the development server.
