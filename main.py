80245fa1276b04ce278c63783ab9e2184ed3707e
anagha zachariah c <anaghazachariah@gmail.com>
2021-04-24 14:51:06 +0530

    update

--- main.py
+++ main.py
@@ -1,17 +1,46 @@
 from flask import Flask 
+import webbrowser
 from flask import render_template,url_for
 from flask import request
 app=Flask(__name__)
+
 @app.route("/",methods=["POST","GET"])
 def home():
+    global mes
+    global xx
     if request.method=="POST":
         x=(request.form)
         for i in x:
             button_clicked=i#NODE WHICH WE CLICKED
         print(button_clicked)
-        return render_template("index.html",cv={'A':['B','C'],'D':['BA','CA']})
+        with open("templates/indexx.html", "r") as f:
+            lines = f.readlines()
+        f.close()
+        with open("templates/indexx.html", "w") as f:
+            for line in lines:
+                if line.strip("\n") != mes:
+                    f.write(line)
+        f.close()
+        f = open('templates/indexx.html','a')
+        mes = """<ul><li><input  type='submit'  class='btn-lg' value='A' name='p'><ul><li><input  type='submit'   class='btn-lg' value='B' name='TRUEE_C'></li><li><input  type='submit'  class='btn-lg' value='C' name='FALSEE_C'></li></ul></li></ul></form></div></div></div> {% endblock %}"""
+        f.write(mes)
+        f.write("\n")
+        f.close()
+        return render_template("indexx.html")
     else:
-        return render_template("index.html",cv={'A':['-','-']})
-
+        f = open('templates/indexx.html','a')
+        mes = """<ul><li><input  type='submit'  class='btn-lg' value='A' name='p'><ul><li><input  type='submit'   class='btn-lg' value='-' name='TRUE_c'></li><li><input  type='submit'  class='btn-lg' value='-' name='FALSE_c'></li></ul></li></ul></form></div></div></div> {% endblock %}"""
+        f.write(mes)
+        f.write("\n")
+        f.close()
+        return render_template("indexx.html")
+mes=''
+xx=0
 if __name__=="__main__":
-   app.run()
+
+   app.run(debug=True)
+
+   
+
+
+
